239b035fbebfdb6ea2e5061a13e7b61cd8e162ed
lazarteyenet <olgaatamari17@gmail.com>
2022-11-21 10:27:41 -0500

    version 03 con diseño

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import { collection, getDocs, query, doc, deleteDoc, where, } from "firebase/firestore";
-import { useState } from "react";
-import firebase, {db} from './componente/firebase';
+import { collection, onSnapshot, where, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import AppForm from "./componente/AppForm";
+import {db} from "./componente/firebase";
 
-function App() {
-  
-  //////////////////// Read  fnRead //////////////////////////
-  const [idActual, setIdActual] = useState(""); 
-  const fnRead = (e) => {
-    console.log("Lectura base de datos"); 
+function App() {  
+  ///////////////////////////////////////////////////////////////////////
+  ////////// READ - fnRead - LECTURA A BD ///////////////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  const [docsBD, setDocsBD] = useState([]);
+  //console.log(docsBD);
+
+  const fnRead = () => {
+    const xColeccionConQuery = query(collection(db, "persona"));
+    //const xColeccionConQuery = query(collection(db, "persona"), where("nombre", "!=", ""));
+    const unsubcribe = onSnapshot(xColeccionConQuery, (xDatosBD) => {
+      const xDoc = [];
+      xDatosBD.forEach( (doc) => {
+        xDoc.push({id: doc.id, ...doc.data()});
+      });
+
+      setDocsBD(xDoc);
+    });
+  }
+
+  fnRead();
+/*
+  useEffect( () => {
+    
+  }, [] );
+  */
+  ///////////////////////////////////////////////////////////////////////
+  ////////// DELETE - fnDelete - Eliminar registros /////////////////////
+  ///////////////////////////////////////////////////////////////////////
+  const [idActual, setIdActual] = useState("");
+
+  const fnDelete = () => {
+    console.log("Se elimino...");
   };
-  //////////////////// Delete  fnDelete //////////////////////
-  const fnDelete = (e) => {
-     console.log("Se elimino con exito...");
-  };
-  
+
   return (
-    <div style={{background:"orange", padding:"10px", width:"350px"}}>
-      <h1>App.js</h1>
-      <AppForm {...{idActual, setIdActual, fnRead}}   />
+    <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
+      <h1>sitiocopia2a3 (App.js)</h1>
+      <h3>READ / DELETE</h3>
+      <AppForm {...{idActual, setIdActual, fnRead}} />
+      {
+        docsBD.map((p) => <p key={p.id}> {p.nombre} </p> )
+      }      
     </div>
   );
 }
